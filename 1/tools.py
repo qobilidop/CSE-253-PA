755095a47821bee6186f051d20787f1255e41e85
Bili Dong <qobilidop@gmail.com>
2017-01-16 11:15:09 -0800

    Refactor trainer

--- 1/tools.py
+++ 1/tools.py
@@ -3,60 +3,70 @@ import pandas as pd
 
 
 class TrainingResults(object):
-    def __init__(self, record, final_weights):
-        self.record = record
-        self.final_weights = final_weights
+    def __init__(self, records, epoch_final):
+        self.records = records
+        self.epoch_final = epoch_final
 
     def plot_loss_function(self):
         plt.figure()
-        plt.plot(self.record['E_train'], label='train')
-        plt.plot(self.record['E_hold'], label='hold-out')
-        plt.plot(self.record['E_test'], label='test')
+        plt.plot(self.records['E_train'], label='train')
+        plt.plot(self.records['E_hold'], label='hold-out')
+        plt.plot(self.records['E_test'], label='test')
+        plt.axvline(self.epoch_final, color='gray',
+                    label='final epoch {}'.format(self.epoch_final))
         plt.legend(loc=0)
         plt.xlabel('epoch')
         plt.ylabel('loss function')
+        plt.axvline()
 
     def plot_percent_correct(self):
         plt.figure()
-        plt.plot(self.record['c_train'], label='train')
-        plt.plot(self.record['c_hold'], label='hold-out')
-        plt.plot(self.record['c_test'], label='test')
+        plt.plot(self.records['c_train'], label='train')
+        plt.plot(self.records['c_hold'], label='hold-out')
+        plt.plot(self.records['c_test'], label='test')
+        plt.axvline(self.epoch_final, color='gray',
+                    label='final epoch {}'.format(self.epoch_final))
         plt.legend(loc=0)
         plt.xlabel('epoch')
         plt.ylabel('percent correct')
 
 
-def train(NN, dss, rate, lam=0, epoch_min=100, epoch_max=None):
-    nn = NN(dss.train.dim)
-    record = pd.DataFrame()
-    weights = []
+def train(nn_class, dss, rate, lam=0, epoch_min=None, epoch_max=None,
+          early_stopping=3):
+    nn = nn_class(dss.train.dim)
+    records = pd.DataFrame()
     epoch = 0
 
     while True:
-        row = pd.Series()
+        record = pd.Series()
 
         nn.use_labeled_images(dss.train)
         nn.update(rate(epoch), lam)
-        weights += [nn.weights]
-        row['E_train'] = nn.loss_function()
-        row['c_train'] = nn.percent_correct()
+        record['weights'] = nn.weights
+        record['E_train'] = nn.loss_function()
+        record['c_train'] = nn.percent_correct()
 
         nn.use_labeled_images(dss.validation)
-        row['E_hold'] = nn.loss_function()
-        row['c_hold'] = nn.percent_correct()
+        record['E_hold'] = nn.loss_function()
+        record['c_hold'] = nn.percent_correct()
 
         nn.use_labeled_images(dss.test)
-        row['E_test'] = nn.loss_function()
-        row['c_test'] = nn.percent_correct()
+        record['E_test'] = nn.loss_function()
+        record['c_test'] = nn.percent_correct()
 
-        record = record.append(row, ignore_index=True)
+        records = records.append(record, ignore_index=True)
         epoch += 1
 
         if (epoch_max is not None) and (epoch > epoch_max):
             break
 
-        if epoch > epoch_min:
-            if (record['E_hold'][-4:].diff()[-3:] > 0).all():
-                break
+        if (epoch_min is None) or (epoch > epoch_min):
+            if early_stopping is not None:
+                s = early_stopping
+                consistent_increase = (records['E_hold'][-(s + 1):]
+                                       .diff()[-s:] > 0).all()
+                if consistent_increase:
+                    break
 
-    return TrainingResults(record, weights[record['E_hold'].argmin()])
+    epoch_final = records['E_hold'].argmin()
+    return TrainingResults(records, epoch_final)
